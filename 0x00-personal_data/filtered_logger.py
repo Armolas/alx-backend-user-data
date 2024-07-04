4f73dc948f35bb2719700c38a57639f810b67d54
Armolas <armolas06@gmail.com>
2024-07-04 22:04:28 +0100

    data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -44,7 +44,7 @@ class RedactingFormatter(logging.Formatter):
         return super(RedactingFormatter, self).format(record)
 
 
-PII_FIELDS = ("ip", "phone", "ssn", "password", "name")
+PII_FIELDS = ("email", "phone", "ssn", "password", "name")
 
 
 def get_logger() -> logging.Logger:
