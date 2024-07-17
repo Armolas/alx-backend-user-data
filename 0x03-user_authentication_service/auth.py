2620262acd6b2801514dc741c70c6b550f539b5a
Armolas <armolas06@gmail.com>
2024-07-18 00:35:19 +0100

    destroy

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,3 +82,15 @@ class Auth:
             return user
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroys a user session
+        """
+        if not user_id:
+            return None
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+            return None
+        except Exception:
+            return None
