22d887782d5e6aaec6e8b0b36b6baba424fa338b
Armolas <armolas06@gmail.com>
2024-07-17 22:46:31 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,11 +8,13 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 AUTH = Auth()
 
+
 @app.route('/')
 def root():
     """ root endpoint
     """
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
@@ -30,7 +32,6 @@ def users():
         return jsonify(
                 {"message": "email already registered"}
                 ), 400
-
 
 
 if __name__ == "__main__":
