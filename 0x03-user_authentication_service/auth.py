90cf061b2695e72d98180004ae9336d67da4e98a
Armolas <armolas06@gmail.com>
2024-07-18 01:18:48 +0100

    ai

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -105,5 +105,5 @@ class Auth:
             user = self._db.find_user_by(email=email)
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        except Exception as e:
-            return None
+        except Exception:
+            raise ValueError
