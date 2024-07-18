d0b23c656725bc2bfc89ad545ee14d8c0fecdebb
Armolas <armolas06@gmail.com>
2024-07-18 01:06:07 +0100

    profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,14 +63,14 @@ def logout():
         abort(403)
 
 
-@app.route('/profile', strict_slashes=False)
+@app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile():
     """ Gets a user profile
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        return jsonify({"email": f"user.email"]), 200
+    if user is not None:
+        return jsonify({"email": f"user.email"}), 200
     else:
         abort(403)
 
